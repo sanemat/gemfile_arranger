ce54126e07ff2b1c383f455e4333c3ec7ae9ef78
sanemat <o.gata.ken@gmail.com>
2014-11-26 22:25:57 +0900

    Clear spike files

--- bin/genfile_arranger.rb
+++ bin/genfile_arranger.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
 require 'gemfile_arranger'
 
-code = ARGF.read
-ast, comments = GemfileArranger::Base.parse(code)
-generated = GemfileArranger::Base.generate(ast, comments)
-puts ast.inspect
-puts generated
--- lib/gemfile_arranger.rb
+++ lib/gemfile_arranger.rb
@@ -1,20 +1,2 @@
-require 'gemfile_arranger/version'
-require 'parser/current'
-require 'unparser'
-
 module GemfileArranger
-  class Base
-    def self.parse(code)
-      ast, comments = Parser::CurrentRuby.parse_with_comments(code)
-      [ast, comments]
-    end
-
-    def self.traverse(ast)
-      ast
-    end
-
-    def self.generate(ast, comments = nil)
-      Unparser.unparse(ast, comments)
-    end
-  end
 end
