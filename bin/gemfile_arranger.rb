e27e7914c24302a711fda6f37e71a548f82dc147
sanemat <o.gata.ken@gmail.com>
2014-12-04 01:53:15 +0900

    Remove unnecessary lines

--- bin/gemfile_arranger.rb
+++ bin/gemfile_arranger.rb
@@ -6,31 +6,9 @@ require 'pp'
 require 'unparser'
 require 'safe_yaml/load'
 
-config = SafeYAML.load_file('config/.gemfile_arranger.base.yml')
+base_config = SafeYAML.load_file('config/.gemfile_arranger.base.yml')
+
 puts config
-
-class RemoveDo < Parser::Rewriter
-  def on_while(node)
-    # Check if the statement starts with "do"
-    if node.location.begin.is?('do')
-      remove(node.location.begin)
-    end
-  end
-end
-
-class SortBlock < Parser::Rewriter
-  def on_send(node)
-    super
-    if node.children[1] == :gem
-      byebug
-      insert_before(node.loc.selector, '1')
-    end
-  end
-
-  def on_begin(node)
-    byebug
-  end
-end
 
 code = <<-EOF
 # comment 1
@@ -50,15 +28,8 @@ buffer        = Parser::Source::Buffer.new('(example)')
 buffer.source = code
 parser        = Parser::CurrentRuby.new
 ast           = parser.parse(buffer)
-rewriter      = RemoveDo.new
-#
-# # Rewrite the AST, returns a String with the new form.
-# puts rewriter.rewrite(buffer, ast)
 
 class SourceProcessor < Parser::AST::Processor
-  def initialize
-  end
-
   def on_begin(node)
     node.updated(:begin, node.children.sort_by{|child| child.children[1]})
     # nodes = process_all(node)
@@ -67,12 +38,5 @@ class SourceProcessor < Parser::AST::Processor
 end
 
 processor = SourceProcessor.new
-# pp processor.process(ast)
 rewrited_ast = processor.process(ast)
 puts Unparser.unparse(rewrited_ast)
-# new_buffer = Parser::Source::Buffer.new('(new)')
-# puts rewriter.rewrite(new_buffer, rewrited_ast)
-# new_source = rewriter.rewrite(new_buffer, rewrited_ast)
-# new_buffer.source = new_source
-#
-# puts new_buffer.source
