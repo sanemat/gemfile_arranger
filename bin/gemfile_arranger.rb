e7e5c9a2eac3375cb502b7f56534ecf00b6accb0
sanemat <o.gata.ken@gmail.com>
2014-12-06 12:47:58 +0900

    Add priority gems

--- bin/gemfile_arranger.rb
+++ bin/gemfile_arranger.rb
@@ -92,8 +92,37 @@ class SortGemInGroupTraverser < Parser::AST::Processor
   end
 end
 
+class SortPriorityGemInGroupTraverser < Parser::AST::Processor
+  def initialize(keys)
+    @keys = Array(keys.dup).map(&:to_s)
+  end
+
+  def on_block(node)
+    sorted_block = sort_gem_in_group_with_keys(node, @keys)
+    node.updated(:block, sorted_block) if node != sorted_block
+  end
+
+  def sort_gem_in_group_with_keys(node, keys)
+    send_node, args_node, body_node = node.children
+    return node if send_node.children[1] != :group
+    return node if body_node.type != :begin
+    gems = body_node.children.sort_by.with_index do |child, i|
+      gem_name = child.children[2].children[0]
+      key_index = keys.index(gem_name) || keys.length
+      [key_index, i]
+    end
+    return node if body_node.children == gems
+
+    sorted_body_node = body_node.updated(:begin, gems)
+    [send_node, args_node, sorted_body_node]
+  end
+end
+
 sort_gem_in_group = SortGemInGroupTraverser.new
 rewrited_ast = sort_gem_in_group.process(ast)
+
+sort_priority_gem_in_group = SortPriorityGemInGroupTraverser.new(CONFIG['priority_gem'])
+rewrited_ast = sort_priority_gem_in_group.process(rewrited_ast)
 
 sort_block = SortBlockTraverser.new(CONFIG['block_order'])
 rewrited_ast = sort_block.process(rewrited_ast)
