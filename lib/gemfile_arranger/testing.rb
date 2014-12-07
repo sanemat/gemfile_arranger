67e323ed225076e3786e7eafbc01ab168189513f
sanemat <o.gata.ken@gmail.com>
2014-12-07 13:28:43 +0900

    Shorten one line

--- lib/gemfile_arranger/testing.rb
+++ lib/gemfile_arranger/testing.rb
@@ -1,7 +1,8 @@
 module GemfileArranger
   module Testing
     # NOTE: Too slow(140X slower).
-    #       0.00487 seconds(ast vs ast) vs 0.678528 seconds(contents vs contents)
+    #       ast:      0.00487 seconds
+    #       contents: 0.678528 seconds
     def contents_to_ast(contents)
       buffer        = Parser::Source::Buffer.new('(contents_to_ast)')
       buffer.source = contents
