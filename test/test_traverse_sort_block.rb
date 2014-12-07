4d61cabd33fbd6cb2e236f9cc40d6970e51cf67f
sanemat <o.gata.ken@gmail.com>
2014-12-07 11:45:17 +0900

    Add actual test

--- test/test_traverse_sort_block.rb
+++ test/test_traverse_sort_block.rb
@@ -3,9 +3,46 @@ require_relative 'helper'
 class TraverseSortBlockTest < Test::Unit::TestCase
   include AST::Sexp
 
-  def test_power_assert
+  test 'Sort by large categories' do
+    # ----
+    # gem 'bar'
+    # gem 'action_args'
+    # source 'https://rubygems.org'
+    # ruby '2.1.5'
+    # ----
+    ast = \
+      s(:begin,
+        s(:send, nil, :gem,
+          s(:str, 'bar')),
+        s(:send, nil, :gem,
+          s(:str, 'action_args')),
+        s(:send, nil, :source,
+          s(:str, 'https://rubygems.org')),
+        s(:send, nil, :ruby,
+          s(:str, '2.1.5')))
+
+    # ----
+    # source 'https://rubygems.org'
+    # ruby '2.1.5'
+    # gem 'bar'
+    # gem 'action_args'
+    # ----
+    ast_expected = \
+      s(:begin,
+        s(:send, nil, :source,
+          s(:str, 'https://rubygems.org')),
+        s(:send, nil, :ruby,
+          s(:str, '2.1.5')),
+        s(:send, nil, :gem,
+          s(:str, 'bar')),
+        s(:send, nil, :gem,
+          s(:str, 'action_args')))
+
+    config = %w(source ruby gemspec gem group)
+    sort_block = GemfileArranger::Traverse::SortBlock.new(config)
+
     assert do
-      "0".class == "3".to_i.times.map {|i| i + 1 }.class
+      sort_block.process(ast) == ast_expected
     end
   end
 end
