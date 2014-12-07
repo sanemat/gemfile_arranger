b5fdd22d7a42e351f9458f98eafbd2216dcea52c
sanemat <o.gata.ken@gmail.com>
2014-12-07 12:28:28 +0900

    Use #contents_to_ast

--- test/test_traverse_sort_block.rb
+++ test/test_traverse_sort_block.rb
@@ -1,13 +1,12 @@
 require_relative 'helper'
+require 'unindent'
 
 class TraverseSortBlockTest < Test::Unit::TestCase
   include AST::Sexp
 
-  def contents_to_ast(contents, as_file = true)
-    code = (as_file) ? '(' + contents + ')' : contents
-
+  def contents_to_ast(contents)
     buffer        = Parser::Source::Buffer.new('(contents_to_ast)')
-    buffer.source = code
+    buffer.source = contents
     parser        = Parser::CurrentRuby.new
     parser.parse(buffer)
   end
@@ -52,6 +51,52 @@ class TraverseSortBlockTest < Test::Unit::TestCase
 
     assert do
       sort_block.process(ast) == ast_expected
+    end
+  end
+
+  test 'Equal contents_to_ast' do
+    contents = <<-EOS.unindent
+      gem 'bar'
+      gem 'action_args'
+      source 'https://rubygems.org'
+      ruby '2.1.5'
+    EOS
+    ast = \
+      s(:begin,
+        s(:send, nil, :gem,
+          s(:str, 'bar')),
+        s(:send, nil, :gem,
+          s(:str, 'action_args')),
+        s(:send, nil, :source,
+          s(:str, 'https://rubygems.org')),
+        s(:send, nil, :ruby,
+          s(:str, '2.1.5')))
+
+    assert do
+      contents_to_ast(contents) == ast
+    end
+  end
+
+  test 'Sort by large categories with contents' do
+    contents = <<-EOS.unindent
+      gem 'bar'
+      gem 'action_args'
+      source 'https://rubygems.org'
+      ruby '2.1.5'
+    EOS
+
+    expected = <<-EOS.unindent
+      source 'https://rubygems.org'
+      ruby '2.1.5'
+      gem 'bar'
+      gem 'action_args'
+    EOS
+
+    config = %w(source ruby gemspec gem group)
+    sort_block = GemfileArranger::Traverse::SortBlock.new(config)
+
+    assert do
+      sort_block.process(contents_to_ast(contents)) == contents_to_ast(expected)
     end
   end
 end
