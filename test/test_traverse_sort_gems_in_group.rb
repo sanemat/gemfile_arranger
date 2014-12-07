bf932b371d657699e88a1113f89308e786b1493b
sanemat <o.gata.ken@gmail.com>
2014-12-07 13:22:46 +0900

    Test no effect

--- test/test_traverse_sort_gems_in_group.rb
+++ test/test_traverse_sort_gems_in_group.rb
@@ -31,4 +31,31 @@ class TraverseSorGemsInGroupTest < Test::Unit::TestCase
       sort_block.process(contents_to_ast(contents)) == contents_to_ast(expected)
     end
   end
+
+  test 'No effect to single gem in group' do
+    contents = <<-EOS.unindent
+      gem 'bar'
+      source 'https://rubygems.org'
+      group :development do
+        gem 'single'
+      end
+      ruby '2.1.5'
+    EOS
+
+    expected = <<-EOS.unindent
+      gem 'bar'
+      source 'https://rubygems.org'
+      group :development do
+        gem 'single'
+      end
+      ruby '2.1.5'
+    EOS
+
+    sort_block = GemfileArranger::Traverse::SortGemsInGroup.new
+
+    assert do
+      sort_block.process(contents_to_ast(contents)) == contents_to_ast(expected)
+    end
+  end
+
 end
