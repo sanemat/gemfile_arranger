1d6661c5255c3e9e60e50391ca59305824531aee
sanemat <o.gata.ken@gmail.com>
2014-12-07 13:11:46 +0900

    Test sort gems in group

---
+++ test/test_traverse_sort_gems_in_group.rb
@@ -0,0 +1,34 @@
+require_relative 'helper'
+
+class TraverseSorGemsInGroupTest < Test::Unit::TestCase
+  include AST::Sexp
+  include GemfileArranger::Testing
+
+  test 'Sort gems in group with contents' do
+    contents = <<-EOS.unindent
+      gem 'bar'
+      source 'https://rubygems.org'
+      group :development do
+        gem 'foo'
+        gem 'baz'
+      end
+      ruby '2.1.5'
+    EOS
+
+    expected = <<-EOS.unindent
+      gem 'bar'
+      source 'https://rubygems.org'
+      group :development do
+        gem 'baz'
+        gem 'foo'
+      end
+      ruby '2.1.5'
+    EOS
+
+    sort_block = GemfileArranger::Traverse::SortGemsInGroup.new
+
+    assert do
+      sort_block.process(contents_to_ast(contents)) == contents_to_ast(expected)
+    end
+  end
+end
