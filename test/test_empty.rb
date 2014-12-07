8732726c527f1d8f58fe41830fa56ea6d759b678
sanemat <o.gata.ken@gmail.com>
2014-12-07 11:45:45 +0900

    Remove fake test

--- test/test_empty.rb
+++
@@ -1,9 +0,0 @@
-require './test/helper'
-
-class AssertionTest < Test::Unit::TestCase
-  def test_power_assert
-    assert do
-      "0".class == "3".to_i.times.map {|i| i + 1 }.class
-    end
-  end
-end
